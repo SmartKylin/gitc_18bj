7f3cbdb4174a12fce90d6dd60c3d109fa709f3e6
wj15510151110 <wj15510151110@gmail.com>
2018-05-08 14:57:47 +0800

    feat(footer):备案号

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,36 +1,45 @@
 import React from 'react'
 import './index.scss'
+import { BackTop } from 'antd';
+import 'antd/dist/antd.css';
+import logo from './images/logo.png'
+
 export default class Footer extends React.Component{
   constructor(props){
     super(props)
   }
   render(){
     return <div className="footer_box">
-      <div className="footer_box_left">
-        <ul>
-          <li>赞助大会&展览展示咨询 :</li>
-          <li>合作单位&合作媒体咨询 :</li>
-          <li>麒麟会会员申请咨询 :</li>
-          <li>票务咨询 :</li>
-          <li>致电我们 :</li>
-        </ul>
-        <ul>
-          <li>business@kylinclub.org</li>
-          <li>gitc@kylinclub.org</li>
-          <li>member@kylinclub.org</li>
-          <li>ticket@kylinclub.org</li>
-          <li>010-86466226</li>
-        </ul>
-      </div>
-      <div className="footer_box_right">
-         <div>
-           GITC
-         </div>
+      <div className="footer_box_A">
+        <div className="footer_box_left">
+          <ul>
+            <li>赞助大会&展览展示咨询 :</li>
+            <li>合作单位&合作媒体咨询 :</li>
+            <li>麒麟会会员申请咨询 :</li>
+            <li>票务咨询 :</li>
+            <li>致电我们 :</li>
+          </ul>
+          <ul>
+            <li>business@kylinclub.org</li>
+            <li>gitc@kylinclub.org</li>
+            <li>member@kylinclub.org</li>
+            <li>ticket@kylinclub.org</li>
+            <li>010-86466226</li>
+          </ul>
+        </div>
+        <div className="footer_box_right">
+          <div>
+            GITC
+          </div>
           <div>
             GITC全球互联网技术大会始终走在互联网技术交流分享的最前端，是目前国内规模最大、资源最成熟的技术峰会，同时也是中国互联网一年一度的行业盛事。
           </div>
+        </div>
       </div>
-
+      <div className="record">
+          <div><img src={logo} alt=""/> </div><div>京CP备14035833号-1   京公网安备11010200322</div>
+      </div>
+      <BackTop/>
     </div>
   }
 
