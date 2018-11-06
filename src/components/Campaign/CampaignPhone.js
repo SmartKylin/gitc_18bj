abbf1129916fb1c6e7fa1aa7741425805d38f0a5
wj15510151110 <wj15510151110@gmail.com>
2018-11-06 10:34:11 +0800

    增加antd样式引入

--- src/components/Campaign/CampaignPhone.js
+++ src/components/Campaign/CampaignPhone.js
@@ -1,13 +1,95 @@
 import React from 'react'
 
-export default class CampaignPhone extends React.Component{
-  constructor(props){
+import './CampaignPhone.scss'
+import {Form, Input,Button} from 'antd';
+
+const FormItem = Form.Item;
+
+class CampaignPhone extends React.Component {
+
+  constructor(props) {
     super(props)
   }
 
-  render(){
-    return <div>
-      CampaignPhone
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const {getFieldDecorator} = this.props.form;
+
+    return <div className='campaign-phone-box'>
+      <div className='campaign-phone-banner'>
+        <img src="" alt=""/>
+      </div>
+      <div className='campaign-phone-selection-rules'>
+
+      </div>
+      <div className='campaign-phone-character'>
+
+      </div>
+      <div className='campaign-phone-enterprise'>
+
+      </div>
+      <div className='campaign-phone-product'>
+
+      </div>
+      <div className='campaign-phone-information'>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem
+              label='姓名'
+          >
+            {getFieldDecorator('name', {
+              rules: [{required: true, message: 'Please input your nickname!', whitespace: true}],
+            })(
+                <Input/>
+            )}
+          </FormItem>
+
+          <FormItem
+              label='公司'
+          >
+            {getFieldDecorator('gs', {
+              rules: [{required: true, message: 'Please input your nickname!', whitespace: true}],
+            })(
+                <Input/>
+            )}
+          </FormItem>
+
+          <FormItem
+              label='职务'
+          >
+            {getFieldDecorator('zw', {
+              rules: [{required: true, message: 'Please input your nickname!', whitespace: true}],
+            })(
+                <Input/>
+            )}
+          </FormItem>
+
+          <FormItem
+              label="手机"
+          >
+            {getFieldDecorator('phone', {
+              rules: [{required: true, message: 'Please input your phone number!'}],
+            })(
+                <Input/>
+            )}
+          </FormItem>
+
+          <FormItem>
+            <Button type="primary" htmlType="submit">提交</Button>
+          </FormItem>
+
+
+        </Form>
+      </div>
     </div>
   }
 }
+
+export default Form.create()(CampaignPhone);
