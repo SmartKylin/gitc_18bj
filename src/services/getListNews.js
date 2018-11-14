372e403eeb7cc028ca40fcab409e0f02ad9aac3e
库库素 <15510254007@163.com>
2018-11-14 17:22:59 +0800

    fix:大会亮点请求接口按顺序接受

--- src/services/getListNews.js
+++ src/services/getListNews.js
@@ -2,7 +2,7 @@ import api from '../api'
 import http from '../fetch'
 
 export function getListImgs(id) {
-  return http.get(api.getImgList + `${id}/list.json?token=9d349496a7dab8f131c620a806e9ec6d`)
+  return http.get(api.getImgList + `${id}/list.json?token=9d349496a7dab8f131c620a806e9ec6d&order_by=-weight`)
 }
 
 export function getImgs(id) {
